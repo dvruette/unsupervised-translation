8825a7b893d67d55d284e7f66f8daf35b4f28750
Dimitri <dimitrivr@icloud.com>
2022-10-20 15:01:28 +0200

    initial commit

---
+++ src/data.py
@@ -0,0 +1,45 @@
+import os
+
+from datasets import inspect_dataset, load_dataset_builder, Split
+from transformers import PreTrainedTokenizer
+
+def get_dataset(dataset_name: bool = "wmt19", stream: bool = True):
+    folder = f"{dataset_name}_scripts"
+    inspect_dataset("wmt19", folder)
+    builder = load_dataset_builder(
+        os.path.join(folder, "wmt_utils.py"),
+        language_pair=("de", "en"),
+        subsets={
+            Split.TRAIN: ["commoncrawl"],
+            # Split.VALIDATION: ["euelections_dev2019"],
+        },
+    )
+
+    if stream:
+        ds = builder.as_streaming_dataset()
+    else:
+        builder.download_and_prepare()
+        ds = builder.as_dataset()
+
+    return ds.with_format("torch")
+
+
+class DataCollatorForSupervisedMT:
+    def __init__(self, src_tokenizer: PreTrainedTokenizer, tgt_tokenizer: PreTrainedTokenizer, src_key="de", tgt_key="en"):
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
+        self.src_key = src_key
+        self.tgt_key = tgt_key
+
+    def __call__(self, features):
+        features = [x["translation"] if "translation" in x else x for x in features]
+        src_text = [x[self.src_key] for x in features]
+        tgt_text = [x[self.tgt_key] for x in features]
+
+        src_tokens = self.src_tokenizer(src_text, padding=True, max_length=512, truncation=True, return_tensors="pt")
+        tgt_tokens = self.tgt_tokenizer(src_text, padding=True, max_length=512, truncation=True, return_tensors="pt")
+        return {
+            "input_ids": src_tokens["input_ids"],
+            "attention_mask": src_tokens["attention_mask"],
+            "labels": tgt_tokens["input_ids"],
+        }
---
+++ src/supervised/train.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+import hydra
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+# add the working directory to $PYTHONPATH
+# needed to make local imports work
+sys.path.append(os.getenv("PWD", "."))
+
+from src.supervised.model import get_model_and_tokenizer
+from src.supervised.model import SupervisedTranslation
+from src.data import get_dataset, DataCollatorForSupervisedMT
+
+
+
+@hydra.main(config_path="../config", config_name="supervised")
+def main(config):
+    # load model
+    autoencoder, src_tokenizer, tgt_tokenizer = get_model_and_tokenizer()
+
+    model = SupervisedTranslation(autoencoder, lr=config.training.learning_rate)
+
+    ds = get_dataset()
+
+    collator = DataCollatorForSupervisedMT(src_tokenizer, tgt_tokenizer)
+
+    train_dl = DataLoader(
+        ds["train"],
+        batch_size=config.training.batch_size,
+        # num_workers=config.data.num_workers,
+        collate_fn=collator,
+        shuffle=False,
+    )
+
+    trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
+    trainer.fit(model, train_dl)
+
+
+if __name__ == "__main__":
+    main()
