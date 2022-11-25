128738bcf6a0f478feea53e4553e9ac78ce291d2
dvruette <dvruette@ethz.ch>
2022-11-25 16:53:30 +0100

    minor changes

--- scripts/bsub_python.sh
+++ scripts/bsub_python.sh
@@ -8,6 +8,7 @@ bsub -J "$1" -n "$N_CPU" -W "$MAX_TIME" -R "rusage[mem=4096,ngpus_excl_p=$N_GPU,
 module load gcc/6.3.0 python_gpu/3.8.5 cuda/11.1.1 eth_proxy
 # Make sure that local packages take precedence over preinstalled packages
 export PYTHONPATH=$HOME/.local/lib/python3.8/site-packages:$PYTHONPATH
+export SCRATCH=$SCRATCH/unsupervised-translation
 python $1
 ENDBSUB
 
