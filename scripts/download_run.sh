d8b3c577ea7b761c07281aef1b728ddaa30b8774
Dimitri <dimitrivr@icloud.com>
2022-11-26 21:29:17 +0100

    fix download script

--- scripts/download_run.sh
+++ scripts/download_run.sh
@@ -3,6 +3,7 @@
 USER=$1
 RUN_ID=$2
 
-RUN_PATH=$(ssh $USER "echo \$(ls \$(find \$SCRATCH -name ums9xxkw | head -1)/checkpoints/*.ckpt | tail -1)")
+RUN_PATH=$(ssh $USER "echo \$(ls \$(find \$SCRATCH -name $RUN_ID | head -1)/checkpoints/*.ckpt | tail -1)")
+echo "Downloading $RUN_PATH as $RUN_ID.ckpt"
 
 scp $USER:$RUN_PATH ./outputs/$RUN_ID.ckpt
